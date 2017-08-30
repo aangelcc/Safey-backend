373ab7726b384c8b43f42ddf9398a9fab1c942ba
00Mugen00 <genericblue36@gmail.com>
2017-08-30 17:41:25 +0200

    Search Method Draft Final Draft

--- app/Http/Controllers/SearchProductController.php
+++ app/Http/Controllers/SearchProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use function Couchbase\defaultDecoder;
 use Illuminate\Http\Request;
 use App\Product;
 class SearchProductController extends Controller
@@ -21,6 +22,22 @@ class SearchProductController extends Controller
         })->whereBetween('price',[$min,$max]);
         if($request->input('store_id')!==NULL){
             $searchQuery->whereIn('store_id',explode(',',$request->input('store_id')));
+        }
+        switch ($request->input('order_by')){
+            case 1:
+                $searchQuery->orderBy('name','asc');
+                break;
+            case 2:
+                $searchQuery->orderBy('name','desc');
+                break;
+            case 3:
+                $searchQuery->orderBy('price','asc');
+                break;
+            case 4:
+                $searchQuery->orderBy('price','desc');
+                break;
+            default:
+                $searchQuery->orderBy('rating','desc');
         }
         return $searchQuery->paginate(5);
     }
